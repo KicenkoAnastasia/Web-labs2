49040d4d31cc35768d52820859283cccac3cdf66
Киценко Анастасия <kicenkoanastasia2004@gmail.com>
2024-09-11 17:46:37 +0700

    Три кавычки

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ app = Flask (__name__)
 
 @app.route("/")
 def start(): 
-    return "<!doctype html>" \
-        "<html>" \
-        "   <body>" \
-        "       <h1>Wev-серверна flask</h1>" \
-        "   </body>" \
-        "</html>"
+    return """<!doctype html>
+        <html>
+           <body>
+               <h1>Wev-серверна flask</h1>
+           </body>
+        </html>"""
