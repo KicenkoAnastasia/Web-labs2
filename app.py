9e49c6240031655eb9b7071991cf168d4a9da498
Киценко Анастасия <kicenkoanastasia2004@gmail.com>
2024-09-11 17:56:57 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask
 app = Flask (__name__)
 
-@app.route("/")
-def start(): 
+
+@app.route("/web")
+def web(): 
     return """<!doctype html>
         <html>
            <body>
                <h1>Wev-серверна flask</h1>
            </body>
+        </html>"""
+
+@app.route("/author")
+def author():
+    name = 'Киценко Анастасия Валерьевна'
+    group = 'ФБИ-21'
+    faculty = 'ФБ'
+
+    return """<!doctype html>
+        <html>
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """ + faculty + """</p>
+            </body>
         </html>"""
