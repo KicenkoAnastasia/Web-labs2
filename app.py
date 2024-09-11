6b21488c5523999e184ae6a5758e06d64ebfff5c
Киценко Анастасия <kicenkoanastasia2004@gmail.com>
2024-09-11 17:40:57 +0700

    Вывод страницы первой лабораторной

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask (__name__)
+
+@app.route("/")
+def start(): 
+    return "<!doctype html>" \
+        "<html>" \
+        "   <body>" \
+        "       <h1>Wev-серверна flask</h1>" \
+        "   </body>" \
+        "</html>"
