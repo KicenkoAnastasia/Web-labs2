a7c5517b48f24aa858480b570488e2c7b59ff964
Киценко Анастасия <kicenkoanastasia2004@gmail.com>
2024-09-20 13:47:36 +0700

    Исправление адресов

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, url_for, redirect
 app = Flask (__name__)
 
 
-@app.route("/web")
+@app.route("/lab1/web")
 def web(): 
     return """<!doctype html>
         <html>
@@ -14,7 +14,7 @@ def web():
             'Content-Type': 'text/plain; charset=utf-8'
             }
 
-@app.route("/author")
+@app.route("/lab1/author")
 def author():
     name = 'Киценко Анастасия Валерьевна'
     group = 'ФБИ-21'
@@ -85,9 +85,9 @@ if __name__ == '__main__':
 
 
 
-@app.route("/info")
+@app.route("/lab1/info")
 def info():
-    return redirect("/author")
+    return redirect("/lab1/author")
 
 @app.route("/lab1/created")
 def created():
