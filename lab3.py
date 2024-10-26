22a0a1582645439ffc2780f098f47e201b54511e
Киценко Анастасия <kicenkoanastasia2004@gmail.com>
2024-10-26 20:08:47 +0700

    Cookie

--- lab3.py
+++ lab3.py
@@ -1,39 +1,38 @@
-from flask import Blueprint, render_template,url_for
-lab3 = Blueprint ('lab3', __name__)
+from flask import Blueprint, render_template, request, make_response, redirect, url_for
 
+lab3 = Blueprint('lab3', __name__)
 
 @lab3.route('/lab3/')
-def lab3_home():
-    css_path = url_for("static", filename="menu.css")
-    return '''
-        <!doctype html>
-        <html>
-            <head>
-                <title>Лабораторная 3</title>
-                <link rel="stylesheet" type="text/css" href="''' + css_path + '''">
-            </head>
-            <body>
-                <header>
-                    <h1>НГТУ | ФБ | WEB-программирование | часть 2</h1>
-                </header>
-                <nav>
-                   <a href="/">
-                        <div class="back"> 
-                        на главную
-                        </div>
-                    </a>
-                </nav>
+def lab():
+    #  значение куки с именем 'name'
+    name = request.cookies.get('name')
+    name_color = request.cookies.get('name_color')
+    return render_template('lab3/lab3.html', name=name, name_color=name_color)
 
-                <h2>Список роутов</h2>
-                <ul>
-                    <li><a href="/"> Главная</a></li>
-                    <li><a href="/lab3/cookie">Куки</a></li>
-                </ul>
+@lab3.route('/lab3/cookie')
+def cookie():
+    # объект ответа
+    resp = make_response(redirect('/lab3/'))  
+    # Установка куки
+    resp.set_cookie('name', 'Alex', max_age=5)
+    resp.set_cookie('age', '20')
+    resp.set_cookie('name_color', 'magenta')
+    return resp  
 
-                <footer>
-                    <p>Киценко Анастасия Валерьевна</p>
-                    <p>ФБИ-21, Факультет ФБ, 2024 год</p>
-                </footer>
-            </body>
-        </html>
-    '''
+#очистка куки 
+@lab3.route('/lab3/del_cookie')
+def del_cookie():
+    resp = make_response(redirect('/lab3/'))
+    resp.delete_cookie('name')
+    resp.delete_cookie('age')
+    resp.delete_cookie('name_color')
+    return resp
+
+@lab3.route('/lab3/form1')
+def form1():
+    user = request.args.get('user')
+    age = request.args.get('age')
+    sex = request.args.get('sex')
+    return render_template('lab3/form1.html',user=user, age=age, sex=sex)
+
+    
