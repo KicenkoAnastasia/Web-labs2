7bfa782edeeb536b547f752542f58f09af54806d
Киценко Анастасия <kicenkoanastasia2004@gmail.com>
2024-11-12 20:40:10 +0700

    Улучшения

--- lab5.py
+++ lab5.py
@@ -8,6 +8,23 @@ from psycopg2.extras import RealDictCursor
 def lab():
     return render_template('/lab5/lab5.html', login=session.get('login'))
 
+def db_connect():
+    conn = psycopg2.connect(
+        host = '127.0.0.1',
+        database = 'kicenko_anastasia_knowledge_base',
+        user = 'kicenko_anastasia_knowledge_base',
+        password ='123'
+    )
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+
+    return conn, cur
+
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
 @lab5.route('/lab5/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
@@ -19,26 +36,18 @@ def register():
     if not (login or password):
         return render_template('lab5/register.html', error='Заполните все поля')
     
-    conn = psycopg2.connect(
-        host = '127.0.0.1',
-        database = 'kicenko_anastasia_knowledge_base',
-        user = 'kicenko_anastasia_knowledge_base',
-        password ='123'
-    )
 
-    cur = conn.cursor(cursor_factory = RealDictCursor)
+    conn, cur = db_connect()
 
     cur.execute(f"SELECT login FROM users WHERE login = '{login}';")
     if cur.fetchone():
-        cur.close()
-        conn.close()
+        db_close(conn, cur)
         return render_template('lab5/register.html',
                                error="Такой пользователь уже существует")
     
     cur.execute(f"INSERT INTO users (login,password) VALUES ('{login}', '{password}');")
-    conn.commit()
-    cur.close()
-    conn.close()
+
+    db_close(conn, cur)
     return render_template('lab5/success.html', login=login)
 
 
@@ -53,40 +62,29 @@ def login():
     if not (login and password):  # Проверка на пустые поля
         return render_template('lab5/login.html', error="Заполните поля")
 
-    # Подключаемся к базе данных
-    conn = psycopg2.connect(
-        host='127.0.0.1',
-        database='kicenko_anastasia_knowledge_base',
-        user='kicenko_anastasia_knowledge_base',
-        password='123'
-    )
-
-    #получение результатов как словарь
-    cur = conn.cursor(cursor_factory=RealDictCursor)
+    # Подключение к базе данных
+    conn, cur = db_connect()
 
     #запрос
     cur.execute("SELECT * FROM users WHERE login = %s;", (login,))
     user = cur.fetchone()
 
     if not user:
-        cur.close()
-        conn.close()
+        db_close(conn, cur)
         return render_template('lab5/login.html', error='Логин и/или пароль неверны')
 
-    # Проверяем пароль
+    # Проверка пароля
     if user['password'] != password:
-        cur.close()
-        conn.close()
+        db_close(conn, cur)
         return render_template('lab5/login.html', 
                                error='Логин и/или пароль неверны')
 
 
     session['login'] = login
-    cur.close()
-    conn.close()
+    db_close(conn, cur)
     return render_template('lab5/success_login.html', login=login)
 
 @lab5.route('/lab5/logout')
 def logout():
     session.pop('login', None)  # Удаляем логин пользователя из сессии
-    return redirect(url_for('lab5.lab'))  # Перенаправляем на главную страницу лабораторной
+    return redirect(url_for('lab5.lab'))  # Перенаправляем на главную страницу 
