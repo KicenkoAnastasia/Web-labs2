b7cb71d26156e773bf72ea087e055d1752dc2dc2
Киценко Анастасия <kicenkoanastasia2004@gmail.com>
2024-12-18 01:26:55 +0700

    сохранение всех изменений

--- app.py
+++ app.py
@@ -1,4 +1,39 @@
-from flask import Flask, url_for, redirect, make_response, render_template, request, session
+
+from flask import Flask, render_template, request, session, url_for, redirect, make_response
+
+import os
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+
+import sys
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))  # Добавляем текущую директорию в sys.path
+
+app = Flask(__name__)
+
+from database import db
+# Конфигурация базы данных
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Выбор базы данных
+app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'nastya_kicenko2_orm'
+    db_user = 'nastya_kicenko2_orm'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "nastya_kicenko2_orm.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+# Инициализация базы данных
+db.init_app(app)
+
+# Регистрация blueprints
 from lab1 import lab1  #подключение эскиза 
 from lab2 import lab2
 from lab3 import lab3
@@ -6,20 +41,24 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
-   
-app = Flask(__name__)
-app.register_blueprint(lab1) 
+from lab8 import lab8
+
+app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
-app.register_blueprint(lab7) 
+app.register_blueprint(lab7)
+app.register_blueprint(lab8)
 
-import os
+# Создание таблиц в базе данных при старте приложения
+with app.app_context():
+    db.create_all()
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
-app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
 
 @app.route('/')
