028f5a9104bc449cd399fa1ce06359833ee26bef
Киценко Анастасия <kicenkoanastasia2004@gmail.com>
2024-12-18 14:56:26 +0700

    обавлено оформление всех страниц, стили и фавиконка

--- lab8.py
+++ lab8.py
@@ -17,30 +17,26 @@ def register():
     if request.method == 'GET':
         return render_template('lab8/register.html')
 
-    # Получаем данные из формы
     login_form = request.form.get('login')
     password_form = request.form.get('password')
 
-    # Проверка на пустые поля
     if not login_form or not password_form:
         flash('Логин и пароль не должны быть пустыми!', 'error')
         return redirect(url_for('lab8.register'))
 
-    # Проверка существования пользователя
     if Users.query.filter_by(login=login_form).first():
         flash('Такой пользователь уже существует!', 'error')
         return redirect(url_for('lab8.register'))
 
-    # Создание нового пользователя
     password_hash = generate_password_hash(password_form)
     new_user = Users(login=login_form, password=password_hash)
     db.session.add(new_user)
     db.session.commit()
 
-    # Автоматический вход пользователя
+    # Автоматический вход
     login_user(new_user)
     flash('Регистрация успешна! Вы вошли в систему.', 'success')
-    return redirect(url_for('lab8.lab'))  # Перенаправляем на главную страницу
+    return redirect(url_for('lab8.lab'))
 
 # Авторизация пользователя
 @lab8.route('/lab8/login/', methods=['GET', 'POST'])
@@ -48,22 +44,19 @@ def login():
     if request.method == 'GET':
         return render_template('lab8/login.html')
 
-    # Получаем данные из формы
     login_form = request.form.get('login')
     password_form = request.form.get('password')
+    remember = 'remember' in request.form  # Запомнить пользователя
 
-    # Поиск пользователя
     user = Users.query.filter_by(login=login_form).first()
 
-    # Проверка пользователя и пароля
     if not user or not check_password_hash(user.password, password_form):
         flash('Неверный логин или пароль!', 'error')
         return redirect(url_for('lab8.login'))
 
-    # Вход пользователя и перенаправление
-    login_user(user)
+    login_user(user, remember=remember)  # Используем remember
     flash('Вы успешно вошли в систему!', 'success')
-    return redirect(url_for('lab8.lab'))  # Перенаправляем на главную страницу
+    return redirect(url_for('lab8.lab'))
 
 # Выход пользователя
 @lab8.route('/lab8/logout/')
@@ -77,7 +70,7 @@ def logout():
 @lab8.route('/lab8/articles/')
 @login_required
 def articles():
-    articles_list = Articles.query.all()
+    articles_list = Articles.query.filter_by(login_id=current_user.id).all()
     return render_template('lab8/articles.html', articles=articles_list, login=current_user.login)
 
 # Создание статьи
@@ -87,16 +80,13 @@ def create_article():
     if request.method == 'GET':
         return render_template('lab8/create_article.html')
 
-    # Получаем данные из формы
     title = request.form.get('title')
     article_text = request.form.get('article_text')
 
-    # Проверка на пустые значения
     if not title or not article_text:
         flash('Заголовок и текст статьи не должны быть пустыми!', 'error')
         return redirect(url_for('lab8.create_article'))
 
-    # Создаем новую статью
     new_article = Articles(
         login_id=current_user.id,
         title=title,
@@ -109,4 +99,37 @@ def create_article():
     db.session.commit()
 
     flash('Статья успешно создана!', 'success')
+    return redirect(url_for('lab8.articles'))
+
+# Редактирование статьи
+@lab8.route('/lab8/edit_article/<int:article_id>', methods=['GET', 'POST'])
+@login_required
+def edit_article(article_id):
+    article = Articles.query.filter_by(id=article_id, login_id=current_user.id).first()
+    if not article:
+        flash('Статья не найдена!', 'error')
+        return redirect(url_for('lab8.articles'))
+
+    if request.method == 'GET':
+        return render_template('lab8/edit_article.html', article=article)
+
+    article.title = request.form.get('title')
+    article.article_text = request.form.get('article_text')
+
+    db.session.commit()
+    flash('Статья успешно обновлена!', 'success')
+    return redirect(url_for('lab8.articles'))
+
+# Удаление статьи
+@lab8.route('/lab8/delete_article/<int:article_id>', methods=['POST'])
+@login_required
+def delete_article(article_id):
+    article = Articles.query.filter_by(id=article_id, login_id=current_user.id).first()
+    if not article:
+        flash('Статья не найдена!', 'error')
+        return redirect(url_for('lab8.articles'))
+
+    db.session.delete(article)
+    db.session.commit()
+    flash('Статья успешно удалена!', 'success')
     return redirect(url_for('lab8.articles'))
